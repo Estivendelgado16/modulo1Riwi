966a2e8a0a6659487bf1b4479164a10c7ba20d9a
Estivendelgado16 <brallamdelgado351@gmail.com>
2025-11-21 08:22:38 -0500

    update

--- M1S3/servicios.py
+++ M1S3/servicios.py
@@ -43,10 +43,36 @@ def buscarProducto():
 
     
 def actualizarProducto():
-    print()
+    if not inventario:
+        print("No hay inventario")
+    else:
+        actualizarProduc = input("Ingresa el producto que quiere actualizar: ").title()
+        for producto in inventario:
+            if producto['nombre'] == actualizarProduc:
+                nuevoNombre = input("ingresa nuevo nombre del producto: ").title()
+                nuevoPrecio = float(input("ingresa el nuevo precio: $ "))
+                nuevaCantidad = int(input("ingresa la nueva cantidad: "))
+                producto['nombre'] = nuevoNombre
+                producto['precio'] = nuevoPrecio
+                producto['cantidad'] = nuevaCantidad
+                print("Producto actualizado")
+                break
+        else:
+            print("Producto no encontrado")
 
 def eliminarProducto():
-    print()
+    if not inventario:
+        print("No hay inventario")
+    else:
+        eliminarProducto = input("Ingresa el producto que quiere eliminar: ").title()
+        for i in enumerate(inventario):
+            if i[1]['nombre'] == eliminarProducto:
+                del inventario[i[0]]
+                print("Producto eliminado")
+                break
+        else:
+            print("Producto no encontrado")
+    
 
 def calcEstadistica():
     print()
